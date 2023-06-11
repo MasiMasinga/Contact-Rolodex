dc5060e7e535306b0f85b4e8222b8af444de23b3
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-06-11 19:13:58 +0200

    feat - add providers to _app.js

---
+++ common/context/StateContext.js
@@ -0,0 +1,41 @@
+import React, { createContext, useEffect, useState } from "react";
+import { useSnackbar } from "notistack";
+
+// Components
+import Slide from "@mui/material/Slide";
+
+export const StateContext = createContext();
+
+const TransitionDown = (props) => {
+    return <Slide {...props} direction="down" />;
+};
+
+export const StateProvider = ({ children }) => {
+    const { enqueueSnackbar } = useSnackbar();
+
+    const [notificationMessage, setNotificationMessage] = useState();
+    const [breadcrumbs, setBreadcrumbs] = useState();
+
+    useEffect(() => {
+        if (notificationMessage) {
+            enqueueSnackbar(notificationMessage.message, {
+                variant: notificationMessage.type,
+                anchorOrigin: {
+                    vertical: "top",
+                    horizontal: "center",
+                },
+                TransitionComponent: TransitionDown,
+            });
+        }
+    }, [notificationMessage, enqueueSnackbar]);
+
+    let value = {
+        breadcrumbs,
+        setBreadcrumbs,
+        setNotificationMessage,
+    };
+
+    return (
+        <StateContext.Provider value={value}>{children}</StateContext.Provider>
+    );
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,54 @@
-import '@/styles/globals.css'
+// Mui
+import { ThemeProvider } from "@mui/material/styles";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Notistack
+import { SnackbarProvider } from "notistack";
+
+// Provider
+import { SessionProvider } from "next-auth/react"
+import { StateProvider } from "@/common/contexts/StateContext";
+
+// Global Styles
+import { Global, css } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Theme
+import { theme } from "@/common/utils/theme";
+
+const GlobalStyle = css`
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    margin: 0;
+  }
+
+  .SnackbarContent-root {
+    .SnackbarItem-message {
+      font-family: "Poppins", sans-serif;
+      font-size: 14px;
+    }
+    .MuiSvgIcon-root {
+      color: white !important;
+    }
+  }
+`;
+
+export default function App({ session, Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider>
+        <SessionProvider session={session}>
+          <StateProvider>
+            <Global styles={GlobalStyle} />
+            <CssBaseline />
+            <Component {...pageProps} />
+          </StateProvider>
+        </SessionProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
